49741a4e0959661757c3844d26eef82a4e49f658
Logan Seely <lz50375@umbc.edu>
2024-04-21 23:43:14 -0400

    Added sorting; sort of fixed genres/platforms showing separate records; added edit menu with most of its functionality; user can edit by selecting some/all of a row and clicking edit game button

--- Unitracker/database.py
+++ Unitracker/database.py
@@ -1,6 +1,6 @@
 # This Python file uses the following encoding: utf-8
 from PySide6.QtSql import QSqlDatabase, QSqlQuery, QSqlQueryModel
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, QSortFilterProxyModel
 import os
 
 
@@ -45,13 +45,23 @@ class database:
         """
         headers = ["Name","Progress", "Hours Played", "Start Date", "End Date", "Total\nAchievements", "Completed\nAchievements", "Series name", "Genre", "Platform"]
         model = QSqlQueryModel()
-        q = "WITH genres AS (SELECT name FROM genre) SELECT * FROM game LEFT JOIN ()"
+        q = "WITH genres AS (SELECT * FROM genre) platforms AS (SELECT * FROM platform)"
         # name, progress, hours_played, start_date, end_date, total_achievements, completed_achievements, series_name, genre.name AS genre
-        #model.setQuery("SELECT * FROM game LEFT JOIN genre ON (game.name == genre.game_name)")
-        model.setQuery("SELECT game.name, progress, hours_played, start_date, end_date, total_achievements, completed_achievements, series_name, genre.name FROM game LEFT JOIN genre ON (game.name == genre.game_name)")
+        #model.setQuery("SELECT game.name, progress, hours_played, start_date, end_date, total_achievements, completed_achievements, series_name, group_concat(genre.name, ',') AS genre_name, group_concat(platform.name, ',') AS platform_name FROM game LEFT JOIN (genre NATURAL JOIN platform) GROUP BY game.name")
+        #model.setQuery("WITH g_genres AS (SELECT * FROM game LEFT JOIN genre ON (game.name == genre.game_name)), g_platform AS (SELECT * FROM game LEFT JOIN platform ON (game.name == platform.game_name)) SELECT * FROM g_genre LEFT JOIN g_platform")
+        #model.setQuery("WITH g_genres AS (SELECT *, group_concat(genre.name, ',') FROM game LEFT JOIN genre ON (game.name == genre.game_name) GROUP BY game.name), p AS (SELECT *, group_concat(platform.name, ',') FROM platform) SELECT * FROM g_genres LEFT JOIN p ON (g_genres.name == p.game_name)")
+
+        # mostly working, just has duplicate genre/platform due to joining...
+        model.setQuery("SELECT game.name, progress, hours_played, start_date, end_date, total_achievements, completed_achievements, series_name, group_concat(genre.name, ',') AS genre_name, group_concat(platform.name, ',') AS platform_name FROM game LEFT JOIN genre ON (game.name == genre.game_name) LEFT JOIN platform ON (game.name == platform.game_name) GROUP BY game.name")
+
+        #model.setQuery("SELECT game.name, progress, hours_played, start_date, end_date, total_achievements, completed_achievements, series_name, genre.name, platform.name FROM game LEFT JOIN genre ON (game.name == genre.game_name) LEFT JOIN platform ON (game.name == platform.game_name)")
         for i in range(len(headers)):
             model.setHeaderData(i, Qt.Horizontal, headers[i])
-        return model
+
+        proxyModel = QSortFilterProxyModel()
+        proxyModel.setSourceModel(model)
+
+        return proxyModel
 
     def add_item(self, item, genres=None, platforms=None):
         """
@@ -60,24 +70,31 @@ class database:
         platforms: list of platforms to add for a game
         """
 
-        # fields = ["name", "progress", "hours_played", "start_date", "end_date", "total_achievements", "completed_achievements", "series_name"]
+        # construct the query string
         query = "INSERT INTO game ("
         for v in item:
             query += v + ", "
 
         query = query [:len(query) - 2] + ")"
         query += " VALUES ("
-        print(query)
         num_fields = len(item)
         for i in range(num_fields):
             if (i != num_fields - 1):
                 query += "?, "
             else:
                 query += "?)"
-        print(query)
-
 
         q_obj = QSqlQuery()
+        # insert series into the series table in case it doesn't exist
+        if ("series_name" in item.keys()):
+            # TODO: need to check for old series and update derived attributes
+
+            q_obj.prepare("INSERT INTO series (name, num_games, total_playtime) VALUES (?, ?, ?)")
+            q_obj.bindValue(0, item["series_name"])
+            q_obj.bindValue(1, 1)
+            q_obj.bindValue(2, item["hours_played"])
+            q_obj.exec()
+
         # q_obj.prepare("INSERT INTO game (name, progress, hours_played, start_date, end_date, total_achievements, completed_achievements, series_name) VALUES (?, ?, ?, ?, ?, ?, ?, ?)")
         q_obj.prepare(query)
 
@@ -106,8 +123,107 @@ class database:
         q_obj.finish()
         return rv
 
-    def edit_item(self, item):
-        pass
+    def edit_item(self, old_name, item, genres, platforms):
+        """
+        old_name: previous name of game
+        item: dict of values to set a game item from, updates the item in the db if it exists and returns the result
+        genres: list of genres to add for a game
+        platforms: list of platforms to add for a game
+        """
+        q_obj = QSqlQuery()
+        # retrieve old record of the game
+        q_obj.prepare("SELECT * FROM game WHERE name=?")
+        q_obj.bindValue(0, old_name)
+        q_obj.exec()
+        q_obj.last()
+        old_game = q_obj.record()
+
+        # construct update query for game table
+        query = "UPDATE game SET "
+        for k in item:
+            query += k + "=?,"
+
+        query = query [:len(query) - 1] + " "
+        query += "WHERE name=?"
+
+        # bind values and execute query
+        q_obj.prepare(query)
+        for i, k in enumerate(item):
+            q_obj.bindValue(i, item[k])
+            if (i == len(item) - 1):
+                q_obj.bindValue(i + 1, item["name"])
+        rv = q_obj.exec()
+        if (not rv):
+            # TODO: display error message?
+            print(self.db.lastError())
+
+        # add new series
+        if ("series_name" in item.keys()):
+            # check if old series is the same as new series and update derived attributes
+            old_series = None
+            if (not old_game.isNull("series_name")):
+                q_obj.prepare("SELECT * FROM series WHERE name=?")
+                q_obj.bindValue(0, old_game.value("series_name"))
+                q_obj.exec()
+                q_obj.next()
+                old_series = q_obj.record()
+
+            # old series = new series so update attributes based on new series
+            if (not old_series is None and (item["series_name"] == old_game.value("series_name"))):
+                q_obj.prepare("UPDATE series SET total_playtime=? WHERE name=?")
+                q_obj.bindValue(0, item("hours_played") + old_series.value("total_playtime") - old_game.value("hours_played"))
+                q_obj.bindValue(1, old_game.value("series_name"))
+                q_obj.exec()
+            # old series and new series are different
+            elif (not old_series is None and (item["series_name"] != old_game.value("series_name"))):
+                # if old series still has at least one game attached, update it
+                if (old_series.value("num_games") - 1 > 0):
+                    q_obj.prepare("UPDATE series SET num_games=?, total_playtime=? WHERE name=?")
+                    q_obj.bindValue(0, old_series.value("num_games") - 1)
+                    q_obj.bindValue(1, old_series.value("total_playtime") - old_game.value("hours_played"))
+                    q_obj.bindValue(2, old_game.value("series_name"))
+                    q_obj.exec()
+                # old series has no games left, delete it
+                else:
+                    q_obj.prepare("DELETE FROM series WHERE name=?")
+                    q_obj.bindValue(0, old_game.value("series_name"))
+                    q_obj.exec()
+            # no old series, so add new series
+            else:
+                q_obj.prepare("INSERT INTO series (name, num_games, total_playtime) VALUES (?, ?, ?)")
+                q_obj.bindValue(0, item["series_name"])
+                q_obj.bindValue(1, 1)
+                q_obj.bindValue(2, item["hours_played"])
+                q_obj.exec()
+
+
+        # TODO: need to replace genre records (delete old ones, insert new ones)
+        # add genres
+        if (genres is not None):
+            for i in range(len(genres)):
+                q_obj.prepare("INSERT INTO genre (game_name, name) VALUES (?, ?)")
+                q_obj.bindValue(0, item["name"])
+                q_obj.bindValue(1, genres[i])
+                rv = q_obj.exec()
+
+        # add platforms
+        if (platforms is not None):
+            for i in range(len(platforms)):
+                q_obj.prepare("INSERT INTO platform (game_name, name) VALUES (?, ?)")
+                q_obj.bindValue(0, item["name"])
+                q_obj.bindValue(1, platforms[i])
+                rv = q_obj.exec()
+
+    def get_game(self, name):
+        """
+        returns a game item given a name
+        """
+        q_obj = QSqlQuery()
+        q_obj.prepare("SELECT * FROM game WHERE name=?")
+        q_obj.bindValue(0, name)
+        q_obj.exec()
+        q_obj.next()
+        return q_obj.record()
 
     def close(self):
         if self.db.isOpen():
--- Unitracker/main.py
+++ Unitracker/main.py
@@ -3,6 +3,7 @@ import sys
 from PySide6.QtWidgets import QApplication, QWidget, QMainWindow, QDockWidget, QLabel
 from PySide6.QtWidgets import QGridLayout, QBoxLayout, QVBoxLayout, QPushButton, QLayout
 from PySide6.QtWidgets import QTableView, QHeaderView, QFrame, QRadioButton, QLineEdit
+from PySide6.QtWidgets import QMessageBox
 from PySide6.QtCore import Qt, QSize, QFile
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtGui import QPalette
@@ -46,6 +47,7 @@ class MainWindow(QMainWindow):
         self.create_games_menu()
         self.load_settings()
         self.load_add()
+        self.load_edit()
 
         test1 = {"name": "Final Fantasy", "progress": 75, "hours_played": 123}
         print(self.db.add_item(test1))
@@ -61,7 +63,7 @@ class MainWindow(QMainWindow):
         games_layout = QVBoxLayout()
         game_table = QTableView()
         self.game_table = game_table
-        model = self.db.get_games()
+        model = self.db.get_games()        
         game_table.setModel(model)
 
         # set view properties
@@ -70,6 +72,8 @@ class MainWindow(QMainWindow):
         #game_table.horizontalHeader().setStretchLastSection(1)
         game_table.verticalHeader().hide()
         game_table.setGridStyle(Qt.SolidLine)
+        game_table.setSortingEnabled(True)
+        game_table.sortByColumn(0, Qt.DescendingOrder) # set default order to descending by game name
 
         title = QLabel()
         title.setText("Games")
@@ -113,6 +117,7 @@ class MainWindow(QMainWindow):
         settings.clicked.connect(self.show_settings)
         games.clicked.connect(self.show_games)
         add_game.clicked.connect(self.show_add)
+        edit_game.clicked.connect(self.show_edit)
 
         self.main_scene.addLayout(nav_bar)
         self.main_scene.setStretchFactor(nav_bar, 0)
@@ -127,35 +132,21 @@ class MainWindow(QMainWindow):
     def set_system(self):
         pass
 
-    def load_settings(self):
+    def update_font(self, size):
         """
-        loads the settings menu and connects functions to buttons
+        update the app's text size based on preset options: small, medium, large
         """
-        if ("settings" not in self.menu_dict):
-            settings_ui = QFile("settings.ui")
-            loader = QUiLoader()
-            s = loader.load(settings_ui)
-            s.hide()
-            if not s:
-                print(loader.errorString())
-                sys.exit(-1)
-            self.main_scene.addWidget(s)
-            self.main_scene.setStretchFactor(s, 1)
-            self.menu_dict["settings"] = s
-
-        set_light = self.menu_dict["settings"].findChild(QRadioButton, "light_theme")
-        set_dark = self.menu_dict["settings"].findChild(QRadioButton, "dark_theme")
-        set_system = self.menu_dict["settings"].findChild(QRadioButton, "system_theme")
-        set_light.clicked.connect(self.set_light)
-        set_dark.clicked.connect(self.set_dark)
-        set_system.clicked.connect(self.set_system)
+        pass
 
     def add_game(self):
+        """
+        constructs the item dict and genre and platform lists, then adds the game and updates the model
+        """
         add = self.menu_dict["add_game"]
         game = {}
         genres = None
         platforms = None
-        # name, progress, hours_played, start_date, end_date, total_achievements, completed_achievements, series_name, genres, platforms
+        # name, progress, hours_played, start_date, end_date, total_achievements, completed_achievements, series_name
         for v in add.fields:
             obj = add.fields[v]
             if (obj is not None):
@@ -175,6 +166,110 @@ class MainWindow(QMainWindow):
         self.db.add_item(game, genres, platforms)
         self.game_table.setModel(self.db.get_games())
 
+    def edit_game(self):
+        """
+        will get the current edit_game's fields and update the tuple in the game table
+        takes a QSqlRecord containing the old version of the game
+        """
+        game = {}
+        genres = []
+        platforms = []
+        edit = self.menu_dict["edit_game"]
+        for v in edit.fields:
+            obj = edit.fields[v]
+            if (obj is not None):
+                f = obj.text()
+                obj.clear()
+                if (v == "genre"):
+                   f = f.split(',')
+                   for i in range(len(f)):
+                       f[i] = f[i].strip()
+                   if (len(f[0]) > 0):
+                       genres = f
+                elif (v == "platform"):
+                    f = f.split(',')
+                    for i in range(len(f)):
+                        f[i] = f[i].strip()
+                    if (len(f[0]) > 0):
+                        platforms = f
+                elif (len(f) > 0):
+                    game[v] = f
+
+        self.db.edit_item(self.old_game_name, game, genres, platforms)
+        self.game_table.setModel(self.db.get_games())
+
+    def delete_game(self, name):
+        """
+        Prompts user for confirmation, then deletes the selected game
+        """
+        pass
+
+    def load_settings(self):
+        """
+        loads the settings menu and connects functions to buttons
+        """
+        if ("settings" not in self.menu_dict):
+            settings_ui = QFile("settings.ui")
+            loader = QUiLoader()
+            s = loader.load(settings_ui)
+            settings_ui.close()
+            if not s:
+                print(loader.errorString())
+                sys.exit(-1)
+            s.hide()
+            self.main_scene.addWidget(s)
+            self.main_scene.setStretchFactor(s, 1)
+            self.menu_dict["settings"] = s
+
+            # connect buttons
+            set_light = s.findChild(QRadioButton, "light_theme")
+            set_dark = s.findChild(QRadioButton, "dark_theme")
+            set_system = s.findChild(QRadioButton, "system_theme")
+            set_light.clicked.connect(self.set_light)
+            set_dark.clicked.connect(self.set_dark)
+            set_system.clicked.connect(self.set_system)
+
+    def load_edit(self):
+        """
+        loads the edit game menu, sets up functionality
+        """
+        if ("edit_game" not in self.menu_dict):
+            # load add_game ui file, then modify it
+            edit_game_ui = QFile("add_game.ui")
+            loader = QUiLoader()
+            edit = loader.load(edit_game_ui)
+            edit_game_ui.close()
+            if not edit:
+                print(loader.errorString())
+                sys.exit(-1)
+            edit.hide()
+
+            self.main_scene.addWidget(edit)
+            self.main_scene.setStretchFactor(edit, 1)
+            self.menu_dict["edit_game"] = edit
+
+            title = edit.findChild(QLabel, "title")
+            title.setText("Edit Game")
+
+            # set up functionality
+            edit = self.menu_dict["edit_game"]
+            edit_game = edit.findChild(QPushButton, "add_game")
+            edit_game.setText("Edit Game")
+
+            edit.fields = {}
+            edit.fields["name"] = edit.findChild(QLineEdit, "name_edit")
+            edit.fields["progress"] = edit.findChild(QLineEdit, "progress_edit")
+            edit.fields["hours_played"] = edit.findChild(QLineEdit, "hours_edit")
+            edit.fields["start_date"] = edit.findChild(QLineEdit, "startdate_edit")
+            edit.fields["end_date"] = edit.findChild(QLineEdit, "enddate_edit")
+            edit.fields["total_achievements"] = edit.findChild(QLineEdit, "total_edit")
+            edit.fields["completed_achievements"] = edit.findChild(QLineEdit, "completed_edit")
+            edit.fields["series_name"] = edit.findChild(QLineEdit, "series_edit")
+
+            edit.fields["genre"] = edit.findChild(QLineEdit, "genre_edit")
+            edit.fields["platform"] = edit.findChild(QLineEdit, "platform_edit")
+
+            edit_game.clicked.connect(self.edit_game)
 
 
     def load_add(self):
@@ -184,30 +279,33 @@ class MainWindow(QMainWindow):
         if ("add_game" not in self.menu_dict):
             add_game_ui = QFile("add_game.ui")
             loader = QUiLoader()
-            a = loader.load(add_game_ui)
-            a.hide()
-            if not a:
+            add = loader.load(add_game_ui)
+            add_game_ui.close()
+
+            if not add:
                 print(loader.errorString())
                 sys.exit(-1)
-            self.main_scene.addWidget(a)
-            self.main_scene.setStretchFactor(a, 1)
-            self.menu_dict["add_game"] = a
-        add = self.menu_dict["add_game"]
-        add_game = add.findChild(QPushButton, "add_game")
-        add.fields = {}
-        add.fields["name"] = add.findChild(QLineEdit, "name_edit")
-        add.fields["progress"] = add.findChild(QLineEdit, "progress_edit")
-        add.fields["hours"] = add.findChild(QLineEdit, "hours_edit")
-        add.fields["start"] = add.findChild(QLineEdit, "startdate_edit")
-        add.fields["end"] = add.findChild(QLineEdit, "enddate_edit")
-        add.fields["total"] = add.findChild(QLineEdit, "total_edit")
-        add.fields["completed"] = add.findChild(QLineEdit, "completed_edit")
-        add.fields["series"] = add.findChild(QLineEdit, "series_edit")
 
-        add.fields["genre"] = add.findChild(QLineEdit, "genre_edit")
-        add.fields["platform"] = add.findChild(QLineEdit, "platform_edit")
+            add.hide()
+            self.main_scene.addWidget(add)
+            self.main_scene.setStretchFactor(add, 1)
+            self.menu_dict["add_game"] = add
 
-        add_game.clicked.connect(self.add_game)
+            add_game = add.findChild(QPushButton, "add_game")
+            add.fields = {}
+            add.fields["name"] = add.findChild(QLineEdit, "name_edit")
+            add.fields["progress"] = add.findChild(QLineEdit, "progress_edit")
+            add.fields["hours_played"] = add.findChild(QLineEdit, "hours_edit")
+            add.fields["start_date"] = add.findChild(QLineEdit, "startdate_edit")
+            add.fields["end_date"] = add.findChild(QLineEdit, "enddate_edit")
+            add.fields["total_achievements"] = add.findChild(QLineEdit, "total_edit")
+            add.fields["completed_achievements"] = add.findChild(QLineEdit, "completed_edit")
+            add.fields["series_name"] = add.findChild(QLineEdit, "series_edit")
+
+            add.fields["genre"] = add.findChild(QLineEdit, "genre_edit")
+            add.fields["platform"] = add.findChild(QLineEdit, "platform_edit")
+
+            add_game.clicked.connect(self.add_game)
 
     def show_games(self):
         if (not self.current_menu == "games"):
@@ -220,6 +318,41 @@ class MainWindow(QMainWindow):
             self.menu_dict[self.current_menu].hide()
             self.current_menu = "add_game"
             self.menu_dict[self.current_menu].show()
+
+    def show_edit(self):
+        """
+        shows the edit game menu if a game row has been selected (and populates its fields with the related data),
+        otherwise tells the user to select a row to edit
+        """
+        selected_indexes = self.game_table.selectionModel().selectedIndexes()
+        selected_rows = sorted(set(i.row() for i in selected_indexes))
+
+        if (len(selected_rows) > 1):
+            msg = QMessageBox()
+            msg.setText("More than one game selected; please select exactly one game for editing.")
+            msg.exec()
+        elif (len(selected_rows) == 0):
+            msg = QMessageBox()
+            msg.setText("No game selected; please select one game for editing.")
+            msg.exec()
+        else:
+            # get game and populate menu fields
+            row = selected_rows[0]
+            game_name = self.game_table.model().index(row, 0).data()
+            game = self.db.get_game(game_name)
+
+            edit = self.menu_dict["edit_game"]
+            for i, f in enumerate(edit.fields):
+                if (game.isNull(f)):
+                    edit.fields[f].setText("")
+                else:
+                    edit.fields[f].setText(str(game.value(i)))
+
+            if (not self.current_menu == "edit_game"):
+                self.menu_dict[self.current_menu].hide()
+                self.current_menu = "edit_game"
+                self.menu_dict[self.current_menu].show()
+                self.old_game_name = game_name
 
     def show_settings(self):
         if (not self.current_menu == "settings"):
